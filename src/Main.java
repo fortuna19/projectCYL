1fec8cbadaa0267969e2edd550f09e762b0c7941
fortuna19 <fortuna1919@gmail.com>
2020-05-16 15:27:59 +0300

    'something'

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main {
-    
+    public static void main(String[] args) throws IOException {
+        String url = "https://kuna.io/api/v2/depth?market=btcuah";
+
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //JsonParser jp = new JsonParser();
+
+        System.out.println(response.toString());
+    }
 }
